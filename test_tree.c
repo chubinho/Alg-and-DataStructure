3591eb88220b4e6844ff1ecfdca45419cdd76e65
chubinho <andrewchubinets07@yandex.ru>
2026-03-02 21:48:47 +0300

    add all tests

--- test_tree.c
+++ test_tree.c
@@ -3,20 +3,21 @@
 #include <stdbool.h>
 #include "tree.h"
 
-
-void test1_insert_empty() {
-    Node* root = NULL;
+void test1_insert_empty()
+{
+    Node *root = NULL;
     root = insert(root, 52);
 
     assert(root != NULL);
     assert(root->value == 52);
-    assert(root -> left == NULL);
+    assert(root->left == NULL);
     assert(root->right == NULL);
     freeTree(root);
 }
-void test2_insert(){
-    Node * root = NULL;
-    root = insert(root,5);
+void test2_insert()
+{
+    Node *root = NULL;
+    root = insert(root, 5);
     root = insert(root, 6);
     root = insert(root, 7);
     assert(root->value == 5);
@@ -25,7 +26,8 @@ void test2_insert(){
     freeTree(root);
 }
 
-void test3_remove_element(){
+void test3_remove_element()
+{
     Node *root = NULL;
     root = insert(root, 7);
     root = insert(root, 10);
@@ -40,16 +42,53 @@ void test3_remove_element(){
     freeTree(root);
 }
 
-bool all_tests(){
+void test4_remove_root()
+{
+    Node *root = NULL;
+    root = insert(root, 10);
+    root = insert(root, 12);
+    root = insert(root, 0);
+    root = insert(root, 5);
+    root = insert(root, 15);
+
+    root = removeTree(root,10);
+    assert(root != NULL && root->value == 12);
+
+    assert(root->left != NULL && root->left->value == 0);
+    assert(root->right != NULL && root->right->value == 15);
+    freeTree(root);
+
+}
+test5_degree_tree(){
+    Node* root = NULL;
+    assert(getTreeDegree(root) == 0);
+
+    root = insert(root,5);
+    root = insert(root, 1);
+    assert(getTreeDegree(root) == 1);
+
+    root = insert(root,2);
+    root = insert(root,6);
+    assert(getTreeDegree(root) == 2);
+    
+}
+
+bool all_tests()
+{
     test1_insert_empty();
     printf("Test 1 complete\n");
     test2_insert();
     printf("Test 2 complete\n");
     test3_remove_element();
     printf("Test 3 complete\n");
+    test4_remove_root();
+    printf("Test 4 complete\n");
+    test5_degree_tree();
+    printf("Test 5 complete\n");
     return true;
 }
 
-int main(){
+int main()
+{
     all_tests();
 }
