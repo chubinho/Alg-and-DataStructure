1e277617b46f374b8ac49bb87d123454024c8a99
chubinho <andrewchubinets07@yandex.ru>
2026-03-02 21:28:10 +0300

    add test 2 and 3

--- test_tree.c
+++ test_tree.c
@@ -14,10 +14,39 @@ void test1_insert_empty() {
     assert(root->right == NULL);
     freeTree(root);
 }
+void test2_insert(){
+    Node * root = NULL;
+    root = insert(root,5);
+    root = insert(root, 6);
+    root = insert(root, 7);
+    assert(root->value == 5);
+    assert(root->right->value == 6);
+    assert(root->right->right->value == 7);
+    freeTree(root);
+}
+
+void test3_remove_element(){
+    Node *root = NULL;
+    root = insert(root, 7);
+    root = insert(root, 10);
+    root = insert(root, 52);
+
+    root = removeTree(root, 10);
+
+    assert(root->value == 7);
+    assert(root->left == NULL);
+    assert(root->right != NULL && root->right->value == 52);
+
+    freeTree(root);
+}
 
 bool all_tests(){
     test1_insert_empty();
-    printf("Test complete");
+    printf("Test 1 complete\n");
+    test2_insert();
+    printf("Test 2 complete\n");
+    test3_remove_element();
+    printf("Test 3 complete\n");
     return true;
 }
 
